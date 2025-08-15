75c530d254b461773ef488350ebd1bf5e87877aa
Roka <roka15941@gmail.com>
2025-08-15 17:54:28 +0300

    Create task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+using namespace std;
+#define ll long long
+#define en endl
+int main(){
+  string x;
+  ll count=0;
+  cout<<" enter a word: ";
+  cin>>x;
+  for(int i=0;i<x.length();i++)
+  {
+    if(x[i]=='a'||x[i]=='e'||x[i]=='i'||x[i]=='o'||x[i]=='u')
+    {
+      count++;
+    }
+  }
+  cout<<count<<en;
+   return 0;
+   
+ }
+   
